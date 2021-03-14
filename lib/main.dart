b8dc5eeda558cfcd4b843d24607ce232874e68a0
Pierluigi Barocci <pierluigi.barocci@nautes.com>
2021-03-14 18:23:06 +0100

    create transaction class

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:expenses_app/transaction.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,11 +10,29 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter App',
       home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  // Dummy data
+  // it's a List of Transaction(s) named transactions
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 18.50,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,11 +40,19 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body: Column(
-        children: [
-          Card(
-            child: Text('CHART'),
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('CHART'),
+              elevation: 5,
+            ),
           ),
           Card(
+            color: Colors.red,
             child: Text('LIST OF TX'),
           )
         ],
---
+++ lib/transaction.dart
@@ -0,0 +1,18 @@
+import 'dart:ffi';
+
+import 'package:flutter/foundation.dart';
+
+class Transaction {
+  String id;
+  String title;
+  double amount;
+  DateTime date;
+
+  // constructor
+  Transaction({
+    @required this.id,
+    @required this.title,
+    @required this.amount,
+    @required this.date,
+  });
+}
