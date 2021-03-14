a282d68fcbfe7130a72f28e49c7c6af917097fb5
Pierluigi Barocci <pierluigi.barocci@nautes.com>
2021-03-14 18:48:52 +0100

    [wip] - general style to Card (container,column, row)

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyHomePage extends StatelessWidget {
     Transaction(
       id: 't2',
       title: 'Weekly Groceries',
-      amount: 18.50,
+      amount: 18.52,
       date: DateTime.now(),
     ),
   ];
@@ -55,7 +55,49 @@ class MyHomePage extends StatelessWidget {
             children: transactions
                 .map(
                   (tx) => Card(
-                    child: Text(tx.title),
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 15,
+                          ),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.purple,
+                              width: 2,
+                            ),
+                          ),
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            tx.amount.toString(),
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple,
+                            ),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              tx.title,
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              tx.date.toString(),
+                              style: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 )
                 .toList(),
